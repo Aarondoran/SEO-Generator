05b6877613c72ab97c057173cccfffbe983634ca
Aaron doran <aarondoran@outlook.ie>
2025-12-26 12:42:55 +0000

    Update navbar logo and GitHub link

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,10 @@
 const navbarHTML = `
 <nav class="navbar">
-  <div class="navbar-logo">Head Generator</div>
+  <div class="navbar-logo">SEO Generator</div>
   <div class="navbar-menu">
     <a href="index.html">Home</a>
     <a href="generate.html">Generator</a>
-    <a href="https://github,com/Aarondoran/SEO-Generator">Github</a>
+    <a href="https://github,com/Aarondoran/seo-generator">Github</a>
     <a href="https://github.com/Aarondoran">Creator</a>
   </div>
 </nav>
